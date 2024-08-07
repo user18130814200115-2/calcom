156535035742a0e021dd0ac6e87731fb36909f9f
Thijs Iris Wester <wester.t@protonmail.com>
2024-08-07 12:55:34 +0200

    Update conversion script to support arbitrary lenth details

--- convert-ics.py
+++ convert-ics.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-
+import arrow
 from ics import Calendar, Event
 import sys
 
 month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 current_month = 0
+index = 0
 events = {}
 
 with open(sys.argv[1]) as calendar_file:
@@ -19,29 +20,34 @@ for event in list(calendar.timeline):
 		current_month = date[0] + '-' + date[1]
 		events[current_month] = {}
 	
+	index += 1
 
-	events[current_month][date[2] + event.name]= {}
-	events[current_month][date[2] + event.name]['date'] = date[2]
-	events[current_month][date[2] + event.name]['name'] = event.name
-	events[current_month][date[2] + event.name]['time'] = time
-	events[current_month][date[2] + event.name]['location'] = event.location
+	events[current_month][index]= {}
+	events[current_month][index]['day'] = date[2]
+	events[current_month][index]['name'] = event.name
+	events[current_month][index]['time'] = time
+	events[current_month][index]['location'] = event.location
+	events[current_month][index]['description'] = event.description
 		
 
 for month in events:
 	data = ''
-	data += month_names[int(month.split('-')[1]) - 1] + ' ' + month.split('-')[0] + '\n'
+	data += arrow.get(month).format('YYYY MMMM\n')
 
 	for day in events[month]:
-		data += events[month][day]['date'] + ':\n'
-		data += '\t' + events[month][day]['name'] + '\n\t'
+		data += events[month][day]['day'] + ':\n\t'
+		data += events[month][day]['name'] + '\n\t'
 
 		if events[month][day]['time'] != '00:00 - 00:00':
-			data += events[month][day]['time'] + '\\\\\\\\'
-		details = events[month][day]['location']
-		if details == None:
-			data += '~\n'
-		else:
-			data+= '; '.join(details.split('\n')) + '\n'
+			data += events[month][day]['time']
+
+		if events[month][day]['location'] != None:
+			data+= '\n\t' + '; '.join(events[month][day]['location'].split('\n'))
+
+		if events[month][day]['description'] != None:
+			data+= '\n\t' + '; '.join(events[month][day]['description'].split('\n'))
+
+		data += '\n'
 
 	with open(month + '.cal', 'w') as file:
 		file.write(data)
